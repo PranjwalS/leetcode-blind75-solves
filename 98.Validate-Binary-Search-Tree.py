d393351172ac7e5915f2c9afedfb3bd523e5b1d9
DinnerDinner <singhpranjwal@gmail.com>
2025-10-17 20:07:24 -0400

    27

---
+++ 98.Validate-Binary-Search-Tree.py
@@ -0,0 +1,16 @@
+def isValidBST(root):
+    if not root:
+            return True
+
+    def check(root, min, max):
+        if not root:
+            return True
+
+        if not (min<root.val<max):
+            return False
+ 
+        
+        return check(root.left, min, root.val) and check(root.right, root.val, max)
+
+   
+    return check(root, float('-inf'), float('inf'))
