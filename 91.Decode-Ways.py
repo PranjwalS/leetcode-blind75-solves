2980b7ec5753a81a008bf0b4144338076a6eca58
DinnerDinner <singhpranjwal@gmail.com>
2025-10-31 11:44:24 -0400

    37

---
+++ 91.Decode-Ways.py
@@ -0,0 +1,22 @@
+def numDecodings(s):
+    dic = {}
+    def helper(i):
+        if i == len(s): return 1
+        if s[i] == '0': return 0
+        if i in dic: return dic[i]
+        
+        count = helper(i+1)
+        if len(s[i:])>=2 and int(s[i: i+2])<=26:
+            count += helper(i+2)
+        
+        dic[i] = count
+        return count
+
+    return helper(0)
+
+
+
+
+
+s='2234'
+print(numDecodings(s))
