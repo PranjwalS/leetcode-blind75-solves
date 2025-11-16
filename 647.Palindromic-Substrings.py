f524e05d6b4f341071e082e37b22f299a5601c18
DinnerDinner <singhpranjwal@gmail.com>
2025-11-15 21:29:47 -0500

    44

--- 647.Palindromic-Substrings.py
+++ 647.Palindromic-Substrings.py
@@ -1,18 +1,17 @@
 def countSubstrings(s):
-    working = list(s)
-    for center in range(1, len(working)-1):
-        left = center-1
-        right = center+1
-        
-        while s[center:right+1] == s[center:left-1:-1]:
-            working.append(s[left: right+1])
-            left-=1
-            right+=1
-    
-    return working
+    count=0
+    for center in range(len(s)):
+        for left, right in [(center, center), (center, center+1)]:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count+=1
+                left -= 1
+                right += 1    
+    return count
 
 
 
 
 print(countSubstrings("racecar")) #3
 print(countSubstrings('aaa')) #6
+print(countSubstrings('abbcc')) #6
+print(countSubstrings('abc')) #6
